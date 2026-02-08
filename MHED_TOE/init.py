7157bf671afb8899539bed6fe30c6098e1597d4a
atomadic <trc484@gmail.com>
2026-02-08 14:11:21 -0800

    Initialize MHED-TOE module with core components

---
+++ MHED_TOE/init.py
@@ -0,0 +1,27 @@
+"""
+MHED-TOE: Monadic-Hex Entropic Dynamics Theory of Everything
+
+Microtubule Yukawa Dynamics Generate E8→SM Fermion Spectrum
+and Orchestrated Objective Reduction Consciousness
+"""
+
+__version__ = "1.0.0"
+__author__ = "Thomas Ralph Colvin IV"
+__license__ = "MIT"
+
+from .yukawa_axon_spectrum import YukawaAxonModel
+from .orch_mt_coherence import OrchORCalculator
+from .iit_holographic_phi import HolographicPhi
+from .causal_set_sprinkling import CausalSetSprinkler
+from .revelation_tensor import RevelationTensor
+from .utils import Constants, validate_simulation
+
+__all__ = [
+    "YukawaAxonModel",
+    "OrchORCalculator",
+    "HolographicPhi",
+    "CausalSetSprinkler",
+    "RevelationTensor",
+    "Constants",
+    "validate_simulation",
+]
